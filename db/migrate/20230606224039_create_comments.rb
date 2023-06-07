746b7cdb01b22842382af26c01bc2ce47882e0b3
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-07 09:25:48 +0000

    Add relationship for foreign key

--- db/migrate/20230606224039_create_comments.rb
+++ db/migrate/20230606224039_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :author_id, null: false, foreign_key: { to_table: users }
-      t.references :post_id, null: false, foreign_key: true
+      t.references :author_id, null: false, foreign_key: { to_table: :users }
+      t.references :post_id, null: false, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
