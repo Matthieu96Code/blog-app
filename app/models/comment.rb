172a8c2522e2f84f9e6daef93b3e35cd4c745101
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-08 15:57:56 +0000

    Update the likes counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
-  def update_comments_counter
-    post.update(comments_counter: post.comments.count)
+  def update_posts_comments_counter
+    post.increment!(:comments_counter)
   end
 end
