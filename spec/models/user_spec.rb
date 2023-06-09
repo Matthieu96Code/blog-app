6b06c3dd5abbfcf23e4373b590caf18b98692d56
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-09 17:32:02 +0000

    Add user unit test part1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model 
+  before :each do    
+    @user = User.create(name: 'Kodjo', photo: 'kodjo.jpg', bio: 'Software Engineer', posts_counter: 2)
+  end
+
+  it 'it should set and validate the name provided' do
+    name = @user.name 
+    expect(name).be eq 'Kodjo'
+  end
+
+  it 'should set the photo provided' do 
+    photo = @user.photo 
+    exepect(photo).to eq 'kodjo.jpg'
+  end
+
+  it 'should set the bio provided' do 
+    bio = @user.bio 
+    exepect(bio).to eq 'Software Engineer'
+  end
+
+  it 'should set the post counter provided' do 
+    posts_counter = @user.posts_counter 
+    exepect(posts_counter).to eq 2
+  end
 end
