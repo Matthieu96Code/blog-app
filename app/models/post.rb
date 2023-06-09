f1abd4b616b5a5a69a31fc3b2ba3e20efe39a156
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-09 08:12:16 +0000

    Validation for unblank username

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :posts_counter, presence: true
+
   after_save :update_posts_counter
 
   def most_recent_comments
