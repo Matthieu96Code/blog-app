9745129d2beebd00c11ccc52f3c8f425e4fc518d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-10 07:57:18 +0000

    Update user test fix typos error in the classes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model 
-  before :each do    
-    @user = User.create(name: 'Kodjo', photo: 'kodjo.jpg', bio: 'Software Engineer', posts_counter: 2)
+RSpec.describe User, type: :model do
+  subject { described_class.new(name: 'Tom', bio: 'Teacher from Mexico.', posts_counter: 0) }
+
+  before { subject.save }
+
+  context 'validating :name' do
+    it 'should not valid if not present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should valid if present' do
+      expect(subject).to be_valid
+    end
   end
 
-  it 'it should set and validate the name provided' do
-    name = @user.name 
-    expect(name).be eq 'Kodjo'
+  context 'validating :posts_counter' do
+    it 'should not valid if less than 0' do
+      subject.posts_counter = -4
+      expect(subject).to_not be_valid
+    end
+
+    it 'should not valid if not integer' do
+      subject.posts_counter = 1.345
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'should set the photo provided' do 
-    photo = @user.photo 
-    exepect(photo).to eq 'kodjo.jpg'
-  end
+  describe "#recent_posts" do
+    it "should return the 3 most recent posts" do
+      5.times do |i|
+        Post.create(author: subject, title: 'greeting', text: 'hello word')
+      end
 
-  it 'should set the bio provided' do 
-    bio = @user.bio 
-    exepect(bio).to eq 'Software Engineer'
-  end
-
-  it 'should set the post counter provided' do 
-    posts_counter = @user.posts_counter 
-    exepect(posts_counter).to eq 2
+      recent_posts = subject.most_recent_posts
+      expect(recent_posts.length).to eq(3)
+    end
   end
 end
