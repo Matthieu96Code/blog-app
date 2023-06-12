a97a05e220f9ddf6717aa0461304716f12efc0e2
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-12 18:46:21 +0000

    Update routes with conventional one

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
-  get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
+  root "users#index"
+
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
