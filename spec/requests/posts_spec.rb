2e1052a69630e5e380c7f1b3da87599604fba625
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-13 18:00:56 +0000

    Add response status correct for posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,5 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
 
+  describe "GET/users/:user_id/posts" do
+    it "returns http success" do
+      get "/users/:user_id/posts"
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+    end
+  end
 
+  describe "GET/users/:user_id/posts/:id" do
+    it "returns http success" do
+      get "/users/:user_id/posts/:id"
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+    end
+  end
 end
