178e5392fdb4928be08d77158d449d82440dba5b
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-13 17:45:53 +0000

    Add user body includes correct placeholder text

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe "Users", type: :request do
       get "/users"
       expect(response).to render_template('index')
     end
+
+    it 'should list all users' do
+      get "/users"
+      expect(response.body).to include('Here is a list of user')
+    end
   end
 
   describe "GET/users/:id" do
@@ -26,6 +31,11 @@ RSpec.describe "Users", type: :request do
     it 'should render users/index template' do
       get "/users/:id"
       expect(response).to render_template('show')
+    end
+
+    it 'should list all users' do
+      get "/users/:id"
+      expect(response.body).to include('show user')
     end
   end
 end
