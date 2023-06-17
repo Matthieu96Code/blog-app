40c2183499f3b4758d150b86c414ea2c51be5bed
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-17 02:23:40 +0000

    Add method new and create to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,22 @@ class PostsController < ApplicationController
     @post_id = params[:id]
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.post.new(post_param)
+    if @post.save
+      redirect_to @post
+    else 
+      render: new, status: :unprocessable_entity
+    end
+  end
+  private
+
+  def post_param
+    params.require(:post).permit(:title, :text)
+  end
 end
