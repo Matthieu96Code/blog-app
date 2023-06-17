7cc2d5d6e11aca14eeee9ca00abcdaf14260a50d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-17 11:37:26 +0000

    Create comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.new(author: current_user, **comment_parameters)
+    if @comment.save
+      redirect_to user_post_path(current_user, post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_parameters
+    params.require(:comment).permit(:text)
+  end
+end
