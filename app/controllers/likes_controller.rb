9aa89904c880d86bc91ba206dc6bfc296b74bf90
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-20 20:42:20 +0000

    Remove current user method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    @like = post.likes.new(author: current_user)
+    @like = post.likes.new(author: post.author)
     if @like.save
-      redirect_to user_post_path(current_user, post)
+      redirect_to user_post_path(post.author, post)
     else
       render :new, status: :unprocessable_entity
     end
