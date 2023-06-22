d3b1b34b480a2c986ddecc57f1ae41c0405a2bfc
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-22 08:28:16 +0000

    Remove commented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,4 @@ class UsersController < ApplicationController
     @id = params[:id]
     @current_user = User.find(params[:id])
   end
-
-  # def user_params
-  #   params.require(:user).permit(:photo, :bio)
-  # end
 end
