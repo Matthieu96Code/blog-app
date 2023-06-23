252163f364b76ab1c17b0ad83c37e491fcc2d84e
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-23 06:26:49 +0000

    Update posts counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
+  after_destroy :update_posts_counter
 
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
@@ -16,6 +17,7 @@ class Post < ApplicationRecord
   private
 
   def update_posts_counter
-    author.increment!(:posts_counter)
+    author.update(posts_counter: author.posts.count)
   end
+
 end
