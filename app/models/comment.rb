9bcc32c5867ab362466ed8e451f599cab76f3c8d
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-23 06:27:09 +0000

    Update comment  counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :update_posts_comments_counter
+  after_destroy :update_posts_comments_counter
 
   private
 
   def update_posts_comments_counter
-    post.increment!(:comments_counter)
+    post.update(comments_counter: post.comments.count)
   end
 end
